94fb0b9b8157e348310ac010fa11170544efbe7f
Acer <Acer@DESKTOP-JS0O9TD>
2023-10-31 11:54:35 +0330

    Add project files.

---
+++ triangle/Program.cs
@@ -0,0 +1,17 @@
+﻿//یاسین عربی
+//دنباله مثلثاتی
+
+int First;
+int Second;
+int Result = 0;
+Console.Write("enter the first num: ");
+First = int.Parse(Console.ReadLine());
+Console.Write("enter the second num: ");
+Second = int.Parse(Console.ReadLine());
+for (int x = First; x <= Second; x++)
+
+{
+    Result = (x * (x + 1)) / 2;
+
+    Console.WriteLine(Result);
+}
